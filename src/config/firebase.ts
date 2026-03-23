6187316c7d6195f9fd3b11e6b7a64a23b5ba12e3
tnorip <tatsuya.norimoto.official@gmail.com>
2026-03-24 00:52:12 +0900

    fix: move firebase config to env variables

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -10,12 +10,12 @@ import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
-  apiKey:            'AIzaSyDelUWgdsa5EfSwX6oxcTmz2tcuVVSze_U',
-  authDomain:        'papa-stand.firebaseapp.com',
-  projectId:         'papa-stand',
-  storageBucket:     'papa-stand.firebasestorage.app',
-  messagingSenderId: '21959260795',
-  appId:             '1:21959260795:web:50ccbb6a6d28eb129640de',
+  apiKey:            process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain:        process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId:         process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket:     process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId:             process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 }
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp()
